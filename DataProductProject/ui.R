400dcaeafa94431a972c59f8dd868f49c45370c8
PLLSG <pohlnlee@singnet.com.sg>
2017-08-18 19:15:39 +0800

    final commit includes pitch slides

--- DataProductProject/ui.R
+++ DataProductProject/ui.R
@@ -9,16 +9,17 @@ library(shinythemes)
 suppressPackageStartupMessages(library(googleVis))
 library(plotly)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 shinyUI(
  fluidPage(
     theme = shinythemes::shinytheme("cerulean"),
     
     # Application title
-    title = "Body Fat Measurements",
+    title = "Body Fat Measurements for Males",
 
     h1("Body Fat Measurements for Males"),
 
+  # Define UI for Body Fat Predictor widget
   fluidRow(
     column(5,
             
@@ -32,8 +33,8 @@ shinyUI(
                 "fat percentage of a typical male with a specified ",
                 "abdomen size (measured as circumference in cm). ")),
             h5(paste0(
-                "This prediction model has been built from physical ",
-                "measurements taken from 252 males."
+                "This prediction model has been built using data ",
+                "from physical measurements of 252 males."
                 )),
             h5("Select the abdomen size using the slider above."),
             br(),
@@ -49,18 +50,19 @@ shinyUI(
         
   hr(),
   
+  # Define UI for BMI Calculator widget
   fluidRow(
     column(5,
                    
         # Section for BMI calculator
         wellPanel(
             titlePanel("BMI Calculator"),
-            sliderInput("wt",
-                        "Weight (in kg):",
-                        min=0,max=200,value=75,step=1),
             sliderInput("ht",
                         "Height (in m):",
                         min=1,max=2.5,value=1.8,step=0.01),
+            sliderInput("wt",
+                        "Weight (in kg):",
+                        min=0,max=200,value=75,step=1),
             h5(paste0(
                 "This BMI Calculator computes the Body Mass Index for",
                 " given weight (in kilograms) and height (in metres)."
